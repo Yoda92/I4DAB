2ee635c6888b5a0223dc99db39d5a827cec3208c
Kristian Lau Jespersen <klj.jep@gmail.com>
2020-03-20 13:57:21 +0100

    dbsets added

--- Assignment2/BottomLayerPersistenceLogic/StudentHelperContext.cs
+++ Assignment2/BottomLayerPersistenceLogic/StudentHelperContext.cs
@@ -1,11 +1,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Assignment2.TopLayer.Domain;
 
 
 namespace Assignment2.BottomLayerPersistenceLogic
 {
     public class StudentHelperContext : DbContext
     {
+
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teacher { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-BL1CI2M;Initial Catalog=HelpRequestDB;Integrated Security=True");
