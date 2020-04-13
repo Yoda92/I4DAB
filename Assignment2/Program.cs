417cce128ed175ba2ea32a8022b4721f2a64ca4e
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-04-13 11:03:13 +0200

    Finished CreateExercise

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -11,40 +11,45 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
-            InitMessage();
             string input;
             while (true)
             {
+                InitMessage();
                 Console.WriteLine("Please enter a command:");
                 input = Console.ReadLine();
                 switch (input)
                 {
                     case "CreateCourse":
-                    {
-                        CreateCourse();
-                        break;
-                    }
+                        {
+                            CreateCourse();
+                            break;
+                        }
                     case "CreateStudent":
-                    {
-                        CreateStudent();
-                        break;
-                    }
+                        {
+                            CreateStudent();
+                            break;
+                        }
                     case "GetHelpRequestsStudent":
-                    {
-                        Console.WriteLine("Enter a students AUID");
-                        GetOpenHelpRequests(System.Console.ReadLine());
-                        break;
-                    }
+                        {
+                            Console.WriteLine("Enter a students AUID");
+                            GetOpenHelpRequests(System.Console.ReadLine());
+                            break;
+                        }
                     case "GetHelpRequestsTeacher":
-                    {
-                        Console.WriteLine("Enter teachers AUID");
-                        break;
-                    }
+                        {
+                            Console.WriteLine("Enter teachers AUID");
+                            break;
+                        }
+                    case "CreateExercise":
+                        {
+                            CreateExercise();
+                            break;
+                        }
                     default:
-                    {
-                        Console.WriteLine("Illegal input! Try again.");
-                        break;
-                    }
+                        {
+                            Console.WriteLine("Illegal input! Try again.");
+                            break;
+                        }
                 }
             }
         }
@@ -117,7 +122,27 @@ namespace Assignment2
 
         static void CreateExercise()
         {
-
+            Exercise newExercise = new Exercise();
+            Console.WriteLine("Please enter lecture number. This is half of a primary key");
+            newExercise.Lecture = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter exercise number. This is the other half of the primary key");
+            newExercise.Number = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please specify where help can be found");
+            newExercise.HelpWhere = Console.ReadLine();
+            using (var _UnitOfWork = new UnitOfWork(new StudentHelperContext()))
+            {
+                foreach (Teacher t in _UnitOfWork.Teachers.GetAll())
+                {
+                    Console.WriteLine($"Name: {t.Name}, AUID: {t.AUID}");
+                }
+            }
+            Console.WriteLine("Please specify the AUID of the teacher who is resposible for this exercise");
+            newExercise.TeacherAUID = Console.ReadLine();
+            using (var _UnitOfWork = new UnitOfWork(new StudentHelperContext()))
+            {
+                _UnitOfWork.Exercises.Add(newExercise);
+                _UnitOfWork.Complete();
+            }
         }
 
         static void CreateHelpRequest()
